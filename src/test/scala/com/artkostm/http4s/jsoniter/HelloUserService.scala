870a3accde941608605f84da3cc7ac9337469ccf
Artsiom <artkostm@gmail.com>
2018-10-25 23:36:19 +0300

    small update

--- src/test/scala/com/artkostm/http4s/jsoniter/HelloUserService.scala
+++ src/test/scala/com/artkostm/http4s/jsoniter/HelloUserService.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class HelloUserService[F[_]: Effect] extends Http4sDsl[F] {
-  private val user = User("Test User", 10)
+  private val user = HelloUserSpec.user
 
   import User.listCodec
   val service: HttpService[F] = HttpService[F] {
@@ -18,8 +18,8 @@ class HelloUserService[F[_]: Effect] extends Http4sDsl[F] {
           user <- req.as[User]
           resp <- Ok(user)
         } yield resp
-      case GET -> Root / "test2" / name => Ok(User(name, 20))
-      case GET -> Root / "test3"        => Ok("hello")
+      case GET -> Root / "test2" / name => Ok(user.copy(name))
+      case GET -> Root / "test3"        => Ok(HelloUserSpec.greeting)
       case GET -> Root / "test4"        => Ok(Future.successful(user))
       case req @ POST -> Root / "test5" =>
         for {
